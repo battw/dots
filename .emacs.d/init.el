a3615e478769d9c617724d838c3c79525cef34c9
Ben Attwood <bendattwood@gmail.com>
2019-06-07 22:10:25 +0000

    Gnome .config/autostart. Font size for my yoga 260

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,10 +23,13 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default))))
+    ("628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+ '(package-selected-packages
+   (quote
+    (which-key try gruvbox-theme flycheck-rust rust-mode racket-mode projectile pdf-tools org-bullets hydra key-chord htmlize flycheck evil dumb-jump counsel company-go company auto-package-update ace-window use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+ )
